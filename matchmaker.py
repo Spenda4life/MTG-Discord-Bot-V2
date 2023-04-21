9c442fdd08eb8f88669c25ebdc347a421f3b154a
noster101 <131319124+noster101@users.noreply.github.com>
2023-04-20 21:23:09 -0400

    Create matchmaker.py

---
+++ matchmaker.py
@@ -0,0 +1,15 @@
+#given players in match, return random decks that are appropriately grouped based on one or more criteria
+
+#who playin
+
+#what they got
+
+#rando
+
+#Check criteria
+#ELO
+#win streak
+#Win record
+#Time since last played
+
+#option to re-randomize
