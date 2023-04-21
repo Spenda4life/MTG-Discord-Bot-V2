6701ae81b5e154ad81170ea172ce0b538efbe87d
Spenda4life <michaelhanselL@gmail.com>
2023-04-21 00:35:00 -0400

    cleaned up elo function

--- deckstats.py
+++ deckstats.py
@@ -1,39 +1,27 @@
-def elo(k_factor, d_factor, ratings):
+def elo(k_factor: int, d_factor: int, ratings: list):
     '''Function takes two or more ratings, with the winner's rating first, and returns new ratings'''
 
-    def multi_ev(ratings):
-        '''returns ev of the first rating against the others'''
-        
-        winner = ratings[0]
-        num_players = len(ratings)
-        possible_matchups = num_players * (num_players - 1) / 2
-
-        # sum EVs for all possible 1v1 matchup with the first rating
-        sum_ev = 0
-        for loser in ratings[1:]:
-            sum_ev += 1 / (1 + 10 ** ((loser - winner) / d_factor))
-
-        # returns a quasi average ev
-        return sum_ev / possible_matchups
-    
-    # This loop will iterate once for each rating in the list
-    # The idea is to shuffle the list so each rating is first once
     new_ratings = []
-    for i in range(len(ratings)):
-        
-        # Get ev for the first rating in the list
-        ev = multi_ev(ratings)
+    for indx, rating in enumerate(ratings):
+        # Basic elo equation is rating + k_factor * (1 - ev)
+        new_ratings.append(round(rating + k_factor * ((indx == 0) - multi_ev(d_factor, ratings, indx))))
 
-        # first is for the winner, the rest are for the losers
-        if i == 0:
-            new = ratings[0] + round(k_factor * (1 - ev))
-        else:
-            new = ratings[0] + round(k_factor * (0 - ev))
-            
-        # add new rating to a list
-        new_ratings.append(new)
-        
-        # move first rating to the end of the list for the next iteration
-        ratings.append(ratings.pop(0))
-    
     return new_ratings
+
+
+def multi_ev(d_factor: int, ratings: list, indx: int):
+        '''returns a psudo average expected value of the indx rating against the others'''
+
+        sum_ev = 0 
+        for i, rating in enumerate(ratings):
+            sum_ev += 1 / (1 + 10 ** ((rating - ratings[indx]) / d_factor)) if i != indx else 0
+
+        return sum_ev / (len(ratings) * (len(ratings) - 1) / 2)
+
+ 
+if __name__=='__main__':
+    ratings = [1500,1600,1700,1800]
+    new = elo(60,400,ratings)
+    print(ratings)
+    print(new)
+    print([x - y for x, y in zip(new, ratings)])
