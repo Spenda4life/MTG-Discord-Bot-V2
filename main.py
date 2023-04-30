c78cd53bb752e3a904647a701bfb639f08f10a4f
Spenda4life <michaelhanselL@gmail.com>
2023-04-30 16:04:12 -0400

    more tinkering

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ import deckstats
 import re
 
 
-# ---------- GLOBAL VARIABLES ---------------
-
-
-players = []
-decks = []
-games = []
-
-
 # ---------- CLASS DEFINITIONS ---------------
 
 
@@ -26,13 +18,13 @@ class Player:
 
 
 class Deck:
-    def __init__(self, owner, commander, decklist):
+    def __init__(self, owner, commander, decklist, rating, wins, losses):
         self.owner = owner
         self.commander = commander
-        self.decklsit = decklist
-        self.rating = 1500
-        self.wins = 0
-        self.losses = 0
+        self.decklist = decklist
+        self.rating = rating
+        self.wins = wins
+        self.losses = losses
 
     def __str__(self):
         return f'{self.commander} ({self.owner})'
@@ -57,9 +49,7 @@ class MyClient(discord.Client):
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
-
-        # load decks from local decks.json file
-        pull_decks_from_database(config.deck_path)
+        startup()
 
     async def setup_hook(self) -> None:
         # Sync the application command with Discord.
@@ -158,12 +148,42 @@ class RegisterGameView(discord.ui.View):
 # ---------- FUNCTION DEFINITIONS ---------------
 
 
-def pull_decks_from_database(path):
-    database = deckstats.read_file(path)
-    for owner, commanders in database.items():
-        for commander in commanders:
-            decks.append(Deck(owner,commander))
-    print(f'Loaded {len(decks)} decks from {path}.')
+def startup():
+    """Called when bot first starts"""
+
+    global players
+    global decks
+    global games
+
+    # players = []
+    # games = []
+
+    # load decks from local json file
+    decks = [Deck(**deck) for deck in deckstats.read_file(config.deck_path)]
+    print(f'Loaded {len(decks)} decks from {config.deck_path}')
+
+
+async def load_decks_from_discord():
+    """Use links in #decklists channel to load decks"""
+
+    channel = client.get_channel(config.decklist_channel)
+    async for message in channel.history(limit=None):
+        # Use the regular expression to find links in each message
+        for link in re.findall(r"(?P<url>https?://[^\s]+)", message.content):
+            links = [deck.decklist for deck in decks]
+            if link in links:
+                print(f'{link} already exists in deck database')
+            else:
+                # if a new link is found, get commander and add new deck
+                commander = deckstats.get_commander_name(link)
+                new_deck = Deck(owner=message.author.name, commander=commander, 
+                                decklist=link, rating=1500, wins=0, losses=0)
+                decks.append(new_deck)
+                print(f'New deck registered: {new_deck}')
+
+    # save decks to json file
+    deckstats.write_file([deck.__dict__ for deck in decks], config.deck_path)
+    return f'Saved {len(decks)} decks to {config.deck_path}'
 
 
 # ---------- DISCORD BOT SLASH COMMANDS ---------------
@@ -174,21 +194,20 @@ client = MyClient()
 
 @client.tree.command()
 async def pull_decks(interaction: discord.Interaction):
-    '''Register decks based on decklist links in #decklists channel'''
-    channel = client.get_channel(config.decklist_channel)
-    async for message in channel.history(limit=None):
-        owner = message.author.name
-        # Use the regular expression to find links in each message
-        links = re.findall(r"(?P<url>https?://[^\s]+)", message.content)
-        for link in links:
-            decks.append(Deck(owner, deckstats.getCommanderName(link), link))
+    """Get decklist links in #decklists channel"""
+
+    await interaction.response.send_message(
+        'Loading decks from #decklists channel. This might take a minute.', 
+        ephemeral=True)
+    response = await load_decks_from_discord()
+    await interaction.followup.send(response, ephemeral=True)
 
 
 @client.tree.command()
 async def game(interaction: discord.Interaction):
     """Create a dropdown menu to record a new 4 player game"""
     view = RegisterGameView()
-    await interaction.response.send_message(view=view, ephemeral=True)
+    await interaction.response.send_message(view=view)
 
 
 if __name__=='__main__':
