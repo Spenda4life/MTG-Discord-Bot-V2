5ea39fe5520ab7fa5d1d963b80b0b51b7bb73c21
noster101 <131319124+noster101@users.noreply.github.com>
2023-05-03 21:40:17 -0400

    Update matchmaker.py

--- matchmaker.py
+++ matchmaker.py
@@ -1,10 +1,19 @@
 #given players in match, return random decks that are appropriately grouped based on one or more criteria
+import random
+import json
 
-#who playin
+random_decks = []
 
-#what they got
+with open("decks.json", "r") as read_file:
+    decks = json.load(read_file)
+#who playing
+players = ['ostertoaster10','bonaparte jones','DrSull','Spenda4life']
 
-#rando
+for x in players:
+    random_decks.append(random.choice([deck['commander'] for deck in decks if deck['owner'] == x]))
+
+# for each shortlist of decks, pick a random item from each
+print(random_decks)
 
 #Check criteria
 #ELO
