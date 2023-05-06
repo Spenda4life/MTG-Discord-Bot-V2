2de0039f3111d6c0e30fd8863a165ec713c19900
Spenda4life <michaelhanselL@gmail.com>
2023-05-05 20:57:26 -0400

    updated simulate.py

--- simulate.py
+++ simulate.py
@@ -1,41 +1,32 @@
 import random
-import string
+import deckstats
 
 
-class Deck():
+def random_decks(decks: list[object], players=None):
+    """Returns a random decks for each player"""
+    if players == None:
+        players = random.sample(list(set([x.owner for x in decks])), k=4)
 
-  def __init__(self, name, weight):
-    self.weight = weight
-    self.wins = 0
-    self.losses = 0
+    return [random.choice([deck for deck in decks if deck.owner == player]) for player in players]
 
 
-def simulate_game():
+def simulate_game(decks: list[object]):
   """Randomly pick 4 decks and select a winner"""
+
+  selected_decks = random_decks(decks)
+  winner = random.choice(selected_decks)
   
-  selected_decks = random.choices(decks, k=4)
-  weights = [deck.weight for deck in selected_decks]
-
-  winner = random.choices(selected_decks,
-                          weights=weights,
-                          k=1)
-
   # record win/loss
   for deck in selected_decks:
-    if deck == winner[0]:
+    if deck == winner:
       deck.wins += 1
     else:
       deck.losses += 1
 
-  return winner
 
+decks = deckstats.load_json_data('decks.json', deckstats.Deck)
+for _ in range(100):
+  simulate_game(decks)
 
-decks = []
-for letter in list(string.ascii_lowercase):
-  decks.append(Deck(letter, random.randint(0, 50)))
-
-for _ in range(50):
-  simulate_game()
-
-for deck in sorted(decks, key=lambda x: x.wins, reverse=True):
-  print(deck.__dict__)
+for indx, deck in enumerate(sorted(decks, key=lambda x: x.wins, reverse=True)):
+  print(indx + 1, deck, deck.wins, deck.losses)
